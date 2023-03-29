cfa7dfe7bdd960bb931b675d8865e1e3a1ffeb96
Kawan-Macioroski <88398066+Kawan-Macioroski@users.noreply.github.com>
2023-03-28 22:40:49 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+function getInputValue() {
+                let senha = document.getElementById("senha").value;
+                const senhaCorreta = "ressurreição";
+                if(senha === senhaCorreta) {
+                    window.location.href = "https://youtu.be/8Qh2wGrmDWA";
+                } else {
+                    alert("3RR4D0!!!");
+                }
+            }
